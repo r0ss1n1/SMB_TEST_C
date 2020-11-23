e0203114de257ae8907d9e15cb4c1d54e8b59021
r0ss1n1 <43396895+r0ss1n1@users.noreply.github.com>
2020-11-24 03:00:45 +1100

    Add files via upload

--- smb_test.c
+++ smb_test.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
 
 int main(void) {
 
 	SOCKET SMB_CONNECTION = INVALID_SOCKET;
 	SMB_CONNECTION = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	sockaddr_in SMB_DEETS;
+	struct sockaddr_in SMB_DEETS;
 
 	SMB_DEETS.sin_family = AF_INET;
 	SMB_DEETS.sin_addr.s_addr = inet_addr("127.0.0.1");
-	SMB_DEETS.sin_port = htons(445);
+	SMB_DEETS.sin_port = htons(135);
 
-	result = connect(SMB_CONNECTION, (SOCKADDR *) &SMB_DEETS, sizeof(SMB_DEETS));
+	int result = connect(SMB_CONNECTION, (SOCKADDR *) &SMB_DEETS, sizeof(SMB_DEETS));
+	if(result == SOCKET_ERROR) {
+		printf("socket error");
+		closesocket(SMB_CONNECTION);
+	}
 
+	closesocket(SMB_CONNECTION);
 
 }
