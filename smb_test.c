fe4a9dbe020395c876058d6cfd6a1583d8afa766
r0ss1n1 <43396895+r0ss1n1@users.noreply.github.com>
2020-11-24 03:35:49 +1100

    Add files via upload

--- smb_test.c
+++ smb_test.c
@@ -5,7 +5,10 @@
 
 int main(void) {
 
-	SOCKET SMB_CONNECTION = INVALID_SOCKET;
+	WSADATA wsaData;
+	int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	
+	SOCKET SMB_CONNECTION;
 	SMB_CONNECTION = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	struct sockaddr_in SMB_DEETS;
 
@@ -13,9 +16,9 @@ int main(void) {
 	SMB_DEETS.sin_addr.s_addr = inet_addr("127.0.0.1");
 	SMB_DEETS.sin_port = htons(135);
 
-	int result = connect(SMB_CONNECTION, (SOCKADDR *) &SMB_DEETS, sizeof(SMB_DEETS));
+	result = connect(SMB_CONNECTION, (SOCKADDR *) &SMB_DEETS, sizeof(SMB_DEETS));
 	if(result == SOCKET_ERROR) {
-		printf("socket error");
+		printf("socket error: %ld", WSAGetLastError());
 		closesocket(SMB_CONNECTION);
 	}
 
